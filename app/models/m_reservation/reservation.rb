a6aae6007ce814be4eff4e0f0db6c95e4668a871
FUNABARA Masao <masao@masoo.jp>
2016-08-09 13:03:42 +0900

    add include? method and exclude? method.

--- app/models/m_reservation/reservation.rb
+++ app/models/m_reservation/reservation.rb
@@ -2,5 +2,21 @@ module MReservation
   class Reservation < ActiveRecord::Base
     has_many :assignments
     has_many :resources, through: :assignments
+    
+    # It is in the range of the specified time.
+    # @param [TimeWithZone] specified time
+    # @return [true] It is included in the range.
+    # @return [false] It is not included in the range.
+    def include?(time_zone)
+      (started_at <= time_zone) and (time_zone <= ended_at)
+    end
+
+    # It isn't in the range of the specified time.
+    # @param [TimeWithZone] specified time
+    # @return [true] It is not included in the range.
+    # @return [false] It is included in the range.
+    def exclude?(time_zone)
+      !(include?(time_zone))
+    end
   end
 end
--- test/models/m_reservation/reservation_test.rb
+++ test/models/m_reservation/reservation_test.rb
@@ -2,8 +2,23 @@ require 'test_helper'
 
 module MReservation
   class ReservationTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    def setup
+      @reservation = m_reservation_reservations(:one)
+    end
+
+    test "TEST include? methods" do
+      assert_equal false, @reservation.include?(Time.zone.local(2016, 4, 5, 13, 59, 59))
+      assert_equal true, @reservation.include?(Time.zone.local(2016, 4, 5, 14, 00, 00))
+      assert_equal true, @reservation.include?(Time.zone.local(2016, 4, 5, 14, 59, 59))
+      assert_equal false, @reservation.include?(Time.zone.local(2016, 4, 5, 15, 00, 00))
+    end
+
+    test "TEST exclude? methods" do
+      assert_equal true, @reservation.exclude?(Time.zone.local(2016, 4, 5, 13, 59, 59))
+      assert_equal false, @reservation.exclude?(Time.zone.local(2016, 4, 5, 14, 00, 00))
+      assert_equal false, @reservation.exclude?(Time.zone.local(2016, 4, 5, 14, 59, 59))
+      assert_equal true, @reservation.exclude?(Time.zone.local(2016, 4, 5, 15, 00, 00))
+    end
+
   end
 end
